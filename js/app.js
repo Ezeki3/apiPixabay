07cb8a84bd8bee7d3997b511add39a0946771247
Ezequiel Lino <sonisombra@gmail.com>
2023-06-27 10:36:12 -0600

    creacion de mensaje de error

--- js/app.js
+++ js/app.js
@@ -17,5 +17,15 @@ function validarFormulario(e){
 }
 
 function mostrarAlerta(mensaje){
-  console.log(mensaje);
+  
+  const alerta = document.createElement('P');
+  alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded',
+  'max-w-lg', 'mx-auto', 'mt-6', 'text-center');
+
+  alerta.innerHTML = `
+    <strong class="font-bold">Error!</strong>
+    <span class="block sm:inline">${mensaje}</span>
+  `;
+
+  formulario.appendChild(alerta);
 }
